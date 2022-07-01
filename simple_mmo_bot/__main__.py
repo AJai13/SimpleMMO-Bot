313cec12fe398fb33bbcbd96e1bb4eff78267f95
Adam <99504751+AJai13@users.noreply.github.com>
2022-07-01 15:34:47 -0300

    Attacking o/
    Co-authored-by: Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>

--- simple_mmo_bot/__main__.py
+++ simple_mmo_bot/__main__.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+from selenium.common.exceptions import NoSuchElementException
+
+BOT_VERIFICATION_BYPASS={"clock": "$2y$10$BFgvOcXaoIvJlkOT1F2KVenCq5OLQYfberNmRb5z/VPfd4BEMeEDG"}
 
 def main():
     driver = get_driver()
@@ -35,11 +38,38 @@ def travel(driver):
     assert "Travel" in driver.title
 
 def take_step(driver):
+    assert "Travel" in driver.title
     element = driver.find_element(By.ID, "step_button")
     
     while True:
         element.click()
         time.sleep(10)
+        attack(driver)
 
+def attack(driver):
+    try: 
+       element = driver.find_element_by_xpath("/html/body/div[1]/div[3]/main/div[2]/div/div[2]/div/div/div[4]/div/div/div[2]/div/span[2]/a[1]")
+       element.click()
+       attack_mob(driver)
+    except NoSuchElementException:
+        return
+
+def attack_mob(driver):
+    assert "Attacking" in driver.title
+    element = driver.find_element(By.ID, "attackButton")
+    
+    while True:
+
+        try:
+            time.sleep(3)
+            button = driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div[3]/a[contains(., 'Close')]")
+            button.click()
+            break
+        except NoSuchElementException:
+            element.click()
+            time.sleep(5)
+
+            
+        
 if __name__ == "__main__":
     main()
